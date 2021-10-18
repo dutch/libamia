fde06ce65b7c92cd237db451f091051b487eee66
Dutch Lamberson <chris@lamberson.online>
2021-10-18 13:41:35 -0700

    adds manpage

--- src/include.mk
+++ src/include.mk
@@ -2,3 +2,5 @@ lib_LTLIBRARIES = %D%/libamia.la
 %C%_libamia_la_SOURCES = %D%/amia.c
 %C%_libamia_la_CPPFLAGS = -I$(top_srcdir)/include
 %C%_libamia_la_LDFLAGS = -version-info 0:0:0
+
+dist_man_MANS = %D%/daemonize.3
